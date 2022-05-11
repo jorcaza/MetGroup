eea36a30a1c367c2d6c70342ea9e096767cce57e
jorcaza <jorcaza2009@gmail.com>
2022-05-11 08:37:37 -0500

    se crea módulo principal y sus rutas

---
+++ src/app/metgroup/metgroup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MetgroupRoutingModule } from './metgroup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MetgroupRoutingModule
+  ]
+})
+export class MetgroupModule { }
