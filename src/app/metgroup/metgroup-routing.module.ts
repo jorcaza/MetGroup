b93849b147540a3eb6f60136bc7fe6c63e314fa7
jorcaza <jorcaza2009@gmail.com>
2022-05-11 10:09:28 -0500

    -->creación de componentes para el crud -->creación de rutas

--- src/app/metgroup/metgroup-routing.module.ts
+++ src/app/metgroup/metgroup-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ViewComponent } from './view/view.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'metgroup', redirectTo: 'metgroup/index', pathMatch: 'full' },
+  { path: 'metgroup/index', component: IndexComponent },
+  { path: 'metgroup/:metgroupId/view', component: ViewComponent },
+  { path: 'metgroup/create', component: CreateComponent },
+  { path: 'metgroup/:metgroupId/edit', component: EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
